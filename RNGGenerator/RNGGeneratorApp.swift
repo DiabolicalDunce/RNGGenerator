6fa5708bb611101eaf0047f69e10b06dd56a8ca6
Jonathan Lau <jlauu48+github@gmail.com>
2021-11-18 12:36:45 -0500

    Initial Commit

---
+++ RNGGenerator/RNGGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RNGGeneratorApp.swift
+//  RNGGenerator
+//
+//  Created by Jonathan Lau on 11/18/21.
+//
+
+import SwiftUI
+
+@main
+struct RNGGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
